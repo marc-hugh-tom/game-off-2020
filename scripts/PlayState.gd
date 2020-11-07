e64426a79b3ffc214369b3dfceed1c0db045f04c
Marc Jones <m4rcj0n35@gmail.com>
2020-11-07 13:34:19 +0000

    Sine easing for day-night fraction

--- scripts/PlayState.gd
+++ scripts/PlayState.gd
@@ -20,15 +20,18 @@ func center_screen():
 		viewport_rect.size/2)
 
 func update_map_scale():
-	var fraction = $HUD/Clock.get_day_night_fraction()
+	var fraction = get_day_night_fraction_easing()
 	var a = (map_scale_noon + map_scale_midnight) / 2
 	var b = abs(a - map_scale_midnight)
 	var map_scale = a + b * fraction
 	$Map.set_scale(Vector2(map_scale, map_scale))
 
 func update_player_speed():
-	var fraction = $HUD/Clock.get_day_night_fraction()
+	var fraction = get_day_night_fraction_easing()
 	var a = (player_speed_noon + player_speed_midnight) / 2
 	var b = abs(a - player_speed_midnight)
 	var player_speed = a + b * fraction * -1
 	$Map/Werewolf.set_speed(player_speed)
+
+func get_day_night_fraction_easing():
+	return(sin($HUD/Clock.get_day_night_fraction() * PI / 2))
