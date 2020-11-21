5576caf5f2880e7bc10ca47b4e663acf52e7e072
Tom Craven <>
2020-11-21 14:35:00 +0000

    villager screaming enhancements

--- nodes/villager/actions/scripts/Flee.gd
+++ nodes/villager/actions/scripts/Flee.gd
@@ -1,25 +1,72 @@
 extends "res://nodes/villager/actions/scripts/Action.gd"
 
+class_name Flee
+
 const FLEE_SOUNDS = [AudioManager.SoundType.ARGH, AudioManager.SoundType.SCREAM]
 
+var current_state = Null.new()
+
+class Null:
+	var flee: Flee
+
+	func process(delta):
+		return self
+
+	func get_label():
+		return ""
+
+class Freeze:
+	var freeze_time = 0.5
+	var flee: Flee
+
+	func _init(in_flee: Flee):
+		self.flee = in_flee
+		flee.get_animation_player().play("idle")
+
+	func process(delta: float):
+		freeze_time -= max(delta, 0)
+		if freeze_time <= 0:
+			return RunAway.new(flee)
+		return self
+
+	func get_label():
+		return "freeze"
+
+class RunAway:
+	var flee: Flee
+	
+	func _init(in_flee: Flee):
+		self.flee = in_flee
+		flee.get_animation_player().play("walk")
+		AudioManager.play_sound(FLEE_SOUNDS[randi() % FLEE_SOUNDS.size()], flee.villager)
+
+	func process(delta: float):
+		var villager = flee.villager
+		var werewolf = villager.werewolf
+		var towards_werewolf = (werewolf.position - villager.position).normalized()
+		var away_from_werewolf = -towards_werewolf * villager.get_run_speed()
+
+		villager.set_rotation_with_delta(villager.position + away_from_werewolf, delta)
+		villager.move_and_slide(away_from_werewolf)
+		
+		return self
+
+	func get_label():
+		return "run away"
+
 func get_label():
-	return "flee"
+	return "flee (" + current_state.get_label() + ")"
 
 func on_enter():
-	AudioManager.play_sound(FLEE_SOUNDS[randi() % FLEE_SOUNDS.size()])
 	villager.set_emotion(Villager.Emotion.CURIOSITY, 0.0)
-	get_animation_player().play("walk")
+	current_state = Freeze.new(self)
 
 func on_exit():
 	villager.set_emotion(Villager.Emotion.CURIOSITY, 0.0)
+	current_state = Null.new()
 
 func process(delta):
-	var werewolf = villager.werewolf
-	var towards_werewolf = (werewolf.position - self.get_parent().position).normalized()
-	var away_from_werewolf = -towards_werewolf * villager.get_run_speed()
-	
-	villager.set_rotation_with_delta(villager.position + away_from_werewolf, delta)
-	villager.move_and_slide(away_from_werewolf)
+	current_state = current_state.process(delta)
 
 func should_activate():
 	if villager == null:
