4a46f7d923f87221d14b7c192871f6fc78c12bb3
Tom Craven <tom.cass.craven@gmail.com>
2020-11-25 21:52:17 +0000

    wip

--- scripts/ExpositionState.gd
+++ scripts/ExpositionState.gd
@@ -3,10 +3,9 @@ extends Node2D
 signal continue_to_game
 
 func _ready():
+	AudioManager.play_music("play")
 	$Column/MarginContainer/MenuButton.connect(
 		"button_up", self, "return_to_menu")
 
 func return_to_menu():
-	AudioManager.stop_music()
-	AudioManager.play_sound("intro_chord")
 	emit_signal("continue_to_game")
