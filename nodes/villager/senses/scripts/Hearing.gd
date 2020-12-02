01641b8c1dfa88e764aaec06912ea10f217c1ddc
Tom Craven <tom.cass.craven@gmail.com>
2020-12-02 21:32:51 +0000

    optimise on_sound

--- nodes/villager/senses/scripts/Hearing.gd
+++ nodes/villager/senses/scripts/Hearing.gd
@@ -8,22 +8,14 @@ func _ready():
 	AudioManager.add_listener(self)
 	
 func on_sound(node):
-	var path: PoolVector2Array = villager.navigation.get_simple_path(
-		villager.position, node.position)
-	if within_hearing_distance(path):
+	if within_hearing_distance(node):
+		var path: PoolVector2Array = villager.navigation.get_simple_path(
+			villager.position, node.position)
 		villager.set_emotion(Villager.Emotion.CURIOSITY, 10.0, path)
 		villager.set_emotion(Villager.Emotion.ANGER, 0.0)
 
-func within_hearing_distance(path: PoolVector2Array):
-	return len(path) >= 2 and path_length_squared(path) < hearing_distance_squared
-
-func path_length_squared(path: PoolVector2Array):	
-	var length = 0
-	for i in len(path) - 1:
-		var a = path[i]
-		var b = path[i + 1]
-		length += a.distance_squared_to(b)
-	return length
+func within_hearing_distance(node):
+	return villager.position.distance_squared_to(node.position) < hearing_distance_squared
 
 func on_die():
 	AudioManager.remove_listener(self)
